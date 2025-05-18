c4d677a2601d790daa23884175bf10efbdfb99b9
Alessandro Gerotto <alessandrogerotto@archeido.com>
2025-05-18 15:31:36 +0200

    feat: initial startup

---
+++ components/nav-secondary.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import * as React from "react";
+import * as R from "ramda";
+import { type Icon } from "@tabler/icons-react";
+
+import {
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+export const NavSecondary = ({
+  items,
+  ...props
+}: {
+  items: {
+    title: string;
+    url: string;
+    icon: Icon;
+  }[];
+} & React.ComponentPropsWithoutRef<typeof SidebarGroup>) => {
+  return (
+    <SidebarGroup {...props}>
+      <SidebarGroupContent>
+        <SidebarMenu>
+          {R.map((item: { title: string; url: string; icon: Icon }) => (
+            <SidebarMenuItem key={item.title}>
+              <SidebarMenuButton asChild>
+                <a href={item.url}>
+                  <item.icon />
+                  <span>{item.title}</span>
+                </a>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))(items)}
+        </SidebarMenu>
+      </SidebarGroupContent>
+    </SidebarGroup>
+  );
+}
