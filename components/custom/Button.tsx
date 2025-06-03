68de9e73f40452c1e9361fe6036fb634d5504581
Alessandro Gerotto <gerotto.alessandro08@gmail.com>
2025-06-03 14:11:38 +0200

    feat: added loading to buttons

--- components/custom/Button.tsx
+++ components/custom/Button.tsx
@@ -1,17 +1,20 @@
 import { Loader2Icon } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import type { ComponentPropsWithoutRef } from "react";
+import { useDashboardSearchContext } from "@/Context/Dashboard";
 
-// Ricava tutte le prop del Button nativo
 type CustomButtonProps = {
   loading?: boolean;
 } & ComponentPropsWithoutRef<typeof Button>;
 
 const CustomButton = ({
-  loading = false,
+  loading: loadingProp,
   children,
   ...props
 }: CustomButtonProps) => {
+  const context = useDashboardSearchContext();
+  const loading = loadingProp ?? context?.loading ?? false;
+
   return (
     <Button disabled={loading} {...props}>
       {loading && <Loader2Icon className="animate-spin" />}
