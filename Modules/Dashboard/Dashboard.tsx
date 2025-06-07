62f80b11c44ce1c5b1cee3010913f713537d9bc2
Alessandro Gerotto <gerotto.alessandro08@gmail.com>
2025-06-07 18:21:43 +0200

    feat: eneanched view for mobile

--- Modules/Dashboard/Dashboard.tsx
+++ Modules/Dashboard/Dashboard.tsx
@@ -66,28 +66,48 @@ const Dashboard = () => {
             },
           ]}
         />
-        <div className="flex-1 flex flex-col gap-4 m-4">
-          <div className="flex-[1] flex gap-4">
-            <SummaryCard
-              title="Total"
-              icon={Banknote}
-              value={R.prop("total", totals)}
-            />
-            <SummaryCard
-              title="Liquidity"
-              icon={PiggyBank}
-              value={R.prop("liquidity", totals)}
-            />
-            <SummaryCard
-              title="Cryptocurrencies"
-              icon={Bitcoin}
-              value={R.prop("crypto", totals)}
-            />
-            <SummaryCard
-              title="ETF"
-              icon={Landmark}
-              value={R.prop("etf", totals)}
-            />
+        <div className="flex-1 flex flex-col gap-4 sm:m-4 mt-4">
+          <div className="flex-[1]">
+            {/* Desktop */}
+            <div className="hidden xl:flex gap-4">
+              <SummaryCard
+                title="Total"
+                icon={Banknote}
+                value={R.prop("total", totals)}
+              />
+              <SummaryCard
+                title="Liquidity"
+                icon={PiggyBank}
+                value={R.prop("liquidity", totals)}
+              />
+              <SummaryCard
+                title="Cryptocurrencies"
+                icon={Bitcoin}
+                value={R.prop("crypto", totals)}
+              />
+              <SummaryCard
+                title="ETF"
+                icon={Landmark}
+                value={R.prop("etf", totals)}
+              />
+            </div>
+            <div className="flex xl:hidden overflow-x-auto gap-4 snap-x snap-mandatory scrollbar-hide">
+              {/* Mobile */}
+              {R.map(({ title, icon, key }) => (
+                <div key={key} className="snap-start flex-shrink-0 w-64">
+                  <SummaryCard
+                    title={title}
+                    icon={icon}
+                    value={R.prop(key, totals)}
+                  />
+                </div>
+              ))([
+                { title: "Total", icon: Banknote, key: "total" },
+                { title: "Liquidity", icon: PiggyBank, key: "liquidity" },
+                { title: "Cryptocurrencies", icon: Bitcoin, key: "crypto" },
+                { title: "ETF", icon: Landmark, key: "etf" },
+              ])}
+            </div>
           </div>
           <div className="flex-[2] bg-green-900">2ª riga (2 parti)</div>
           <div className="flex-[2] bg-blue-900">3ª riga (2 parti)</div>
