fcc9ddcca41b638d2456329ff61f7cc822cf913f
Alessandro Gerotto <gerotto.alessandro08@gmail.com>
2025-06-08 17:00:21 +0200

    feat: table

--- Models/Dashboard/table.tsx
+++ Models/Dashboard/table.tsx
@@ -2,40 +2,64 @@
 
 import { Asset } from "@/app/types/dashboard";
 import { Badge } from "@/components/ui/badge";
+import { Progress } from "@/components/ui/progress";
 import { ColumnDef } from "@tanstack/react-table";
 import { useTranslations } from "next-intl";
 
 export function useAssetColumns(): ColumnDef<Asset>[] {
-  const t = useTranslations("generic.categories");
+  const t = useTranslations("");
 
   return [
     {
       accessorKey: "label",
-      header: "Label",
+      header: t("dashboard.table.header.label"),
+      cell: ({ row }) => {
+        const label = row.getValue<string>("label");
+        return <code className="font-bold">{label}</code>;
+      },
     },
     {
       accessorKey: "category",
-      header: "Category",
+      header: t("dashboard.table.header.category"),
       cell: ({ row }) => {
         const category = row.getValue<string>("category");
-        return <Badge variant="outline">{t(category)}</Badge>;
+        return (
+          <Badge variant="outline">{t(`generic.categories.${category}`)}</Badge>
+        );
       },
     },
     {
       accessorKey: "price",
-      header: "Price",
+      header: t("dashboard.table.header.price"),
+      cell: ({ row }) => {
+        const price: number = row.getValue<number>("price");
+        return <p>{`${price.toFixed(2)} €`}</p>;
+      },
     },
     {
       accessorKey: "quantity",
-      header: "Quantity",
+      header: t("dashboard.table.header.quantity"),
     },
     {
       accessorKey: "value",
-      header: "Value",
+      header: t("dashboard.table.header.value"),
+      cell: ({ row }) => {
+        const value: number = row.getValue<number>("value");
+        return <p>{`${value.toFixed(2)} €`}</p>;
+      },
     },
     {
       accessorKey: "percentage",
-      header: "Percentage",
+      header: t("dashboard.table.header.percentage"),
+      cell: ({ row }) => {
+        const percentage: number = row.getValue<number>("percentage");
+        return (
+          <div className="flex items-center gap-4 justify-start w-[200px]">
+            <p className="w-16 text-right">{`${percentage.toFixed(2)} %`}</p>
+            <Progress value={percentage} className="w-40" />
+          </div>
+        );
+      },
     },
   ];
 }
