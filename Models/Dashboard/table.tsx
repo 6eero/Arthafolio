0d577530cce8f0d6736a135ece27b109977119b7
Alessandro Gerotto <gerotto.alessandro08@gmail.com>
2025-06-08 14:33:30 +0200

    feat: using badge in the table

--- Models/Dashboard/table.tsx
+++ Models/Dashboard/table.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Asset } from "@/app/types/dashboard";
+import { Badge } from "@/components/ui/badge";
 import { ColumnDef } from "@tanstack/react-table";
 
 export const columns: ColumnDef<Asset>[] = [
@@ -11,6 +12,9 @@ export const columns: ColumnDef<Asset>[] = [
   {
     accessorKey: "category",
     header: "Category",
+    cell: ({ row }) => {
+      return <Badge variant="outline">{row.getValue("category")}</Badge>;
+    },
   },
   {
     accessorKey: "price",
