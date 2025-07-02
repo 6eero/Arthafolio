6a2048fb712ea35dc3eb6939175517a8ef46b70a
Alessandro Gerotto <gerotto.alessandro08@gmail.com>
2025-07-03 00:14:09 +0200

    fix: login mobile

--- Modules/App/Login/Challenges/Main.tsx
+++ Modules/App/Login/Challenges/Main.tsx
@@ -16,6 +16,7 @@ import { Formik } from "formik";
 import FormikInput from "@/components/formik/Input";
 import Alert from "@/components/error/Alert";
 import { useTranslations } from "next-intl";
+import Logo from "@/components/icons/Logo";
 
 const LoginMain = () => {
   const { onLogin } = useAppActions();
@@ -32,7 +33,7 @@ const LoginMain = () => {
       }}
     >
       {(formik) => (
-        <form onSubmit={formik.handleSubmit}>
+        <form className="" onSubmit={formik.handleSubmit}>
           <Card className="w-[450px] hidden sm:block">
             <CardHeader className="mb-6">
               <CardTitle>{t("title")}</CardTitle>
@@ -69,6 +70,45 @@ const LoginMain = () => {
               </div>
             </CardContent>
           </Card>
+
+          <div className="sm:hidden w-screen p-6">
+            {error && <Alert className="mb-4" error={error} />}
+            <div className="flex flex-col gap-10">
+              <div className="flex flex-col items-center gap-4">
+                <Logo bgColor="var(--color-card-2)" className="w-12 h-12" />
+                <p className="leading-none font-semibold">{t("title")}</p>
+                <p className="text-muted-foreground text-sm">
+                  {t("description")}
+                </p>
+              </div>
+
+              <div className="grid gap-3">
+                <FormikInput
+                  name="email"
+                  label={"login.fields.email.label"}
+                  placeholder={"login.fields.email.placeholder"}
+                  formik={formik}
+                />
+              </div>
+              <div className="">
+                <FormikInput
+                  name="password"
+                  label={"login.fields.password.label"}
+                  placeholder={"login.fields.password.placeholder"}
+                  formik={formik}
+                />
+              </div>
+              <div className="flex flex-col gap-3">
+                <CustomButton
+                  loading={loading}
+                  type="submit"
+                  className="w-full"
+                >
+                  {t("login")}
+                </CustomButton>
+              </div>
+            </div>
+          </div>
         </form>
       )}
     </Formik>
