9b3f921be526c91281a663b4c78a06ba02c65e4d
Alessandro Gerotto <gerotto.alessandro08@gmail.com>
2025-07-19 17:54:56 +0200

    feat: who am i in the settings page

--- Modules/Settings/Settings.tsx
+++ Modules/Settings/Settings.tsx
@@ -1,19 +1,27 @@
 "use client";
 
 import Header from "@/components/layout/Header";
+import { ResourceLoader } from "@/components/layout/ResourceLoader";
 import ThemeSwitcher from "@/components/layout/ThemeSwitcher";
 import { useTranslations } from "next-intl";
 
 const Settings = () => {
   const t = useTranslations("");
+
   return (
-    <div className="w-full flex flex-col">
-      <Header title={t("settings.title")} />
-      <div className="sm:m-4 mt-4 flex items-center gap-4">
-        <p>Theme:</p>
-        <ThemeSwitcher />
+    <ResourceLoader>
+      <div className="w-full flex flex-col">
+        <Header title={t("settings.title")} />
+        <div className="sm:m-4 mt-4 flex items-center gap-4">
+          <p>Theme:</p>
+          <ThemeSwitcher />
+        </div>
+        <div className="sm:m-4 mt-4 flex items-center gap-4">
+          <p></p>
+          <ThemeSwitcher />
+        </div>
       </div>
-    </div>
+    </ResourceLoader>
   );
 };
 
--- components/layout/ResourceLoader.tsx
+++ components/layout/ResourceLoader.tsx
@@ -1,31 +1,33 @@
 "use client";
 
 import * as R from "ramda";
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, createContext } from "react";
 import Loading from "./Loading";
 import { ErrorBlock } from "../error/ErrorBlock";
 import { useAppActions } from "@/api/App/tasks";
 
+const DummyContext = createContext({ loading: false, error: null });
+
 type ResourceLoaderProps = {
   onLoad?: () => void | Promise<void>;
+  context?: React.Context<any>;
   children: React.ReactNode;
-  context: React.Context<any>;
 };
 
 export const ResourceLoader = ({
   onLoad,
   children,
-  context: Context,
+  context,
 }: ResourceLoaderProps) => {
-  const state = useContext(Context);
+  const state = useContext(context || DummyContext);
   const { onWhoAmI } = useAppActions();
-  const { loading = false, error = false } = state;
+  const { loading, error } = state;
 
   useEffect(() => {
     if (!R.isNil(onLoad)) {
-      onWhoAmI();
       onLoad();
     }
+    onWhoAmI();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
