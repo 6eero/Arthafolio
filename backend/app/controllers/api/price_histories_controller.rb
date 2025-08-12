725decc3397f3ba9bec0a97d73d2fb600efee31e
Alessandro Gerotto <gerotto.alessandro08@gmail.com>
2025-08-12 15:55:30 +0200

    feat: added price hisotru controller

---
+++ backend/app/controllers/api/price_histories_controller.rb
@@ -0,0 +1,21 @@
+module Api
+  class CryptoPriceHistoriesController < ApplicationController
+    before_action :authenticate_request
+
+    def index
+      label = params[:label]
+      range = (params[:range] || 30).to_i # giorni, default 30
+
+      data = CryptoPriceHistory
+               .where(label: label)
+               .where('retrieved_at >= ?', range.days.ago)
+               .order(:retrieved_at)
+               .pluck(:retrieved_at, :price)
+
+      render json: {
+        label: label,
+        history: data.map { |t, p| { time: t, price: p.to_f } }
+      }
+    end
+  end
+end
